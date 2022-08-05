aa243bd9833705b1223d29f36497fa5ce6b61fd1
liuxinghua <liuxinghua@visioninsight.com>
2022-08-05 18:22:45 +0800

    redux在react中的应用

--- 前端项目/src/reducer/todoListReducer.js
+++ 前端项目/src/reducer/todoListReducer.js
@@ -7,11 +7,19 @@ const todoListReducer = createSlice({
     },
     reducers: {
         addValue(state, action){
-            console.log('state', state, 'action', action);
+            console.log('state', state.value, 'action', action);
             // state.value += action.payload; 但是好像state希望不变的吧
-            state.value += action.payload;
+            state.value += action.payload; //这样的写法是对原来state的值进行了修改,而reducer规则当中的第2条是不允许对state进行修改也就是要保证纯函数的形式
+            //这里官网给出的解释是@reduxjs/toolkit的createSlice在内部使用了immer库 => 这个库可以使我们更好的编写不可变的逻辑
+        },
+        decrementValue(state, action){
+            console.log('state', state.value, 'action', action);
+            return {
+                ...state.value,
+                value : state.value - action.payload,
+            }
         }
     }
 })
-export const {addValue} = todoListReducer.actions;
+export const {addValue, decrementValue} = todoListReducer.actions;
 export default todoListReducer.reducer;
--- 前端项目/src/view/touchByMiatask/index.js
+++ 前端项目/src/view/touchByMiatask/index.js
@@ -1,13 +1,12 @@
 import React, { useEffect, useCallback} from 'react'
 import { useDispatch, useSelector } from 'react-redux'
-import { addValue } from '../../reducer/todoListReducer'
+import { addValue, decrementValue} from '../../reducer/todoListReducer'
 const Css = require('./index.scss')
 // import url from '../../image/components/TouchByMiatask/二维码.jpeg' //引入jpeg方式的图片不成功
 const Index = () => {
     // const url = require('../../image/components/TouchByMiatask/二维码.jpeg')
     const dispatch = useDispatch();
-    const state = useSelector((state) => state.todoList);
-    console.log('state', state);
+    const {value} = useSelector((state) => state.todoList);
     let preventDefaultFn = useCallback((e) => {e.preventDefault();}, [])
     useEffect(() => {
         document.addEventListener('contextmenu', preventDefaultFn)
@@ -20,10 +19,21 @@ const Index = () => {
     const add = () => {
         dispatch(addValue(10));
     }
+    const decerment = () => {
+        dispatch(decrementValue(5));
+    }
     return (
         <div className={Css['page']}>
             <img src={'https://pic.90sheji.com/design/01/24/80/47/5906e618b27fb.png!/fw/280/quality/90/unsharp/true/compress/true/canvas/280x297a0a0/cvscolor/FFFFFFFF'}></img>
-            <button className={Css['button']} onClick={add}>+</button>
+            <span>{value}</span>
+            <div className={Css['buttonContent']}>
+                <button className={Css['button']} onClick={add}>+</button>
+                <button className={Css['button']} onClick={decerment}>-</button>
+            </div>
+            <div className={Css['redux-title']}>
+                redux的核心就是reducer函数需是纯函数的形式:即不会修改输入的值,不产生异步等的副作用,计算值只靠输入的参数进行计算(即不受外界的影响)
+                具体规则: 1.不会修改传入的state,保证不可变性,每次返回值都需满足当前形式{'{...state, value:action.payload}'},2.内部计算的值只会用到state和action这两者不会受外部的影响,3.函数中不能存在异步等的有副作用的逻辑.
+            </div>
         </div>
     )
 }
