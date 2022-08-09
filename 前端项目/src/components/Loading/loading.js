9b01d2ed1ab02a1036f5ab9fb6aedea2a55d45ef
liuxinghua <liuxinghua@visioninsight.com>
2022-08-09 09:34:21 +0800

    loading遮罩通过原生dom操作的方式&css的style.cssText属性

--- 前端项目/src/components/Loading/loading.js
+++ 前端项目/src/components/Loading/loading.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React, { useEffect, useRef} from 'react'
 import './loading.scss'
 const Css = require('./loading.scss')
 
 const LoadingIndex = () => {
+    useEffect(() => {
+        //初始加载的时候增加一个遮罩
+        useRef.current = document.createElement('div');
+        useRef.current.textContent = '添加遮罩';
+        // dom.style.cssText会一次性重写dom的行内样式值(你用了cssText之后原有的行内样式消失,然后变成你添加的这个)
+        useRef.current.style.cssText = 'width:100vw;height:100vh;background-color:black;position:absolute;top:0;left:0;opacity:0.6;'
+        console.log(useRef.current.style);
+        document.body.appendChild(useRef.current);
+        return () => {
+            document.body.removeChild(useRef.current);
+        }
+    }, [])
     return(
         <>
             <div className={Css['loading-content']}>
