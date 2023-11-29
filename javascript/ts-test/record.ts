438dc7387cd6ed6761681d59e9b7bb71d3f20a69
liuxinghua <liuxinghua@visioninsight.com>
2023-11-29 18:00:54 +0800

    feat:增加Record相关使用能力

--- javascript/ts-test/record.ts
+++ javascript/ts-test/record.ts
@@ -8,3 +8,14 @@ interface TObject {
 }
 
 type RecordT = Record<keyof TObject, IPeopleC> //Record的第一个参数要能通过in操作(也就是type定义的联合类型)
+
+interface RecordString1 {
+    [x: string]: any
+}
+type RecordString = Record<string, number | string>
+const symbolA: Symbol = Symbol(1);
+const recordA: RecordString = {
+    1: 123,
+    '2': 456,
+    symbolA: 789
+}
