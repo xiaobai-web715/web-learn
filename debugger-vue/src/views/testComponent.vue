ec2a72792774628ce3c142cd9d027e6606927801
刘兴华 <liuxinghua@edianyun.com>
2025-11-08 10:04:53 +0800

    ✨ feat: 调试vue源码

---
+++ debugger-vue/src/views/testComponent.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>doubleCount is : {{ doubleCount }}</div>
+  <button @click="updateCount">点击增加功能</button>
+</template>
+<script lang="ts" setup>
+import { ref, computed } from 'vue'
+debugger
+console.log("开始执行testComputed的值")
+const count = ref(0)
+const doubleCount = computed(() => count.value * 2)
+function updateCount() {
+  count.value++;
+}
+</script>
---
+++ debugger-vue/vite.config.ts
@@ -0,0 +1,25 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+import path from 'path'
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      'vue': path.resolve(__dirname, 'public/vue.esm-browser.js')
+    },
+  },
+  optimizeDeps: {
+    exclude: ['vue']
+  }
+})
