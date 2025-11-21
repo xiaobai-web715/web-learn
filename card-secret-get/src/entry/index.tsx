23deba48e1a24fb65d70f1373a56131b31ead330
liuxinghua <liuxinghua715@163.com>
2025-11-21 21:51:35 +0800

    ✨ feat: 首页和游戏介绍移动端化

--- card-secret-get/src/entry/index.tsx
+++ card-secret-get/src/entry/index.tsx
@@ -29,16 +29,19 @@ const GamesEntry = () => {
         navigate(`/receive?tag=${tag}`);
     };
     return (
-        <div className="entry-page bg-cover bg-center bg-no-repeat text-white">
+        <div className="entry-page overflow-y-scroll bg-cover bg-center bg-no-repeat text-white">
             <div className="grid-style grid h-screen">
-                <div className="pt-6">
-                    <div className="font-kai text-center text-[40px] font-bold">GAMERS STORE</div>
-                    <div className="text-center text-[1.5rem] font-bold mt-2">
+                <div className="pt-[0.24rem]">
+                    <div className="font-kai text-center text-[0.4rem] font-bold">GAMERS STORE</div>
+                    <div className="mt-[0.08rem] text-center text-[0.24rem] font-bold">
                         <div className="font-tai">点击游戏图标查看详情介绍</div>
-                        <div className="font-kai text-[1rem]">Click on the game icon to view the details</div>
+                        <div className="font-kai text-[0.16rem]">
+                            Click on the game icon to view the details
+                        </div>
                     </div>
                 </div>
-                <div className="flex items-start justify-center">
+                {/* flex flex-wrap items-start justify-center */}
+                <div className="grid grid-cols-2 [@media(max-width:900px)]:grid-cols-1">
                     {myGamesList.map((item, index) => {
                         return (
                             // <div key={index} className="relative">
@@ -52,7 +55,7 @@ const GamesEntry = () => {
                             //             onClick={() => gotoInterPage(item.tag)}
                             //         > */}
                             //             {/* <div className="box-shadow-[0_0_60px_4px_rgba(0,0,0,0.5)] overflow-inherit absolute inset-0 w-full -rotate-90 transform rounded-lg border-8 border-r-0 border-t-0 border-gray-700 shadow-lg group-hover:border-red-600">
-                                            
+
                             //             </div> */}
                             //             {/* <div className="box-shadow-[0_0_60px_4px_rgba(0,0,0,0.5)] absolute inset-0 left-[-10%] top-[30%] h-full w-[160%] rounded-[5px] border-l-4 border-r-4 border-gray-700 shadow-lg group-hover:border-[#da7b2c]"></div> */}
                             //             <div className="box-shadow-[0_0_60px_4px_rgba(0,0,0,0.5)] border-l-6 border-r-6 absolute inset-0 left-[-6.5%] top-[15%] h-full w-[178%] rounded-[5px] border-gray-700 shadow-lg group-hover:border-[#16b9ff]"></div>
@@ -89,26 +92,33 @@ const GamesEntry = () => {
                             //         </div>
                             //     </div>
                             // </div>
-                            <div key={index}>
-                                <div className="relative w-[300px] border-3 border-gray-600 cursor-pointer border-box group ml-32 mr-32" key={index} onClick={() => gotoInterPage(item.tag)}>
-                                    <div className="absolute top-0 left-0 w-10 h-10 border-t-4 border-l-4 border-blue-400 opacity-0  -translate-x-6 -translate-y-5 transition-all duration-500 ease-out group-hover:opacity-100 group-hover:blur-0 group-hover:-translate-x-3 group-hover:-translate-y-2"></div>
-                                    <div className="absolute top-0 right-0 w-10 h-10 border-t-4 border-r-4 border-blue-400 opacity-0  translate-x-6 -translate-y-5 transition-all duration-500 ease-out group-hover:opacity-100 group-hover:blur-0 group-hover:translate-x-3 group-hover:-translate-y-2"></div>
-                                    <div className="absolute bottom-0 right-0 w-10 h-10 border-b-4 border-r-4 border-blue-400 opacity-0  translate-x-6 translate-y-5 transition-all duration-500 ease-out group-hover:opacity-100 group-hover:blur-0 group-hover:translate-x-3 group-hover:translate-y-2"></div>
-                                    <div className="absolute bottom-0 left-0 w-10 h-10 border-b-4 border-l-4 border-blue-400 opacity-0  -translate-x-6 translate-y-5 transition-all duration-500 ease-out group-hover:opacity-100 group-hover:blur-0 group-hover:-translate-x-3 group-hover:translate-y-2"></div>
+                            <div
+                                key={index}
+                                className="[@media(max-width:900px)]:mb-[0.2rem] [@media(max-width:900px)]:mt-[0.2rem]"
+                            >
+                                <div
+                                    className="border-box group relative m-auto w-[3rem] cursor-pointer border-[0.03rem] border-gray-600 [@media(max-width:750px)]:w-[70%]"
+                                    key={index}
+                                    onClick={() => gotoInterPage(item.tag)}
+                                >
+                                    <div className="absolute left-0 top-0 h-[0.4rem] w-[0.4rem] -translate-x-[0.24rem] -translate-y-[0.2rem] border-l-[0.04rem] border-t-[0.04rem] border-blue-400 opacity-0 transition-all duration-500 ease-out group-hover:-translate-x-[0.12rem] group-hover:-translate-y-[0.08rem] group-hover:opacity-100 group-hover:blur-0"></div>
+                                    <div className="absolute right-0 top-0 h-[0.4rem] w-[0.4rem] -translate-y-[0.2rem] translate-x-[0.24rem] border-r-[0.04rem] border-t-[0.04rem] border-blue-400 opacity-0 transition-all duration-500 ease-out group-hover:-translate-y-[0.08rem] group-hover:translate-x-[0.12rem] group-hover:opacity-100 group-hover:blur-0"></div>
+                                    <div className="absolute bottom-0 right-0 h-[0.4rem] w-[0.4rem] translate-x-[0.24rem] translate-y-[0.2rem] border-b-[0.04rem] border-r-[0.04rem] border-blue-400 opacity-0 transition-all duration-500 ease-out group-hover:translate-x-[0.12rem] group-hover:translate-y-[0.08rem] group-hover:opacity-100 group-hover:blur-0"></div>
+                                    <div className="absolute bottom-0 left-0 h-[0.4rem] w-[0.4rem] -translate-x-[0.24rem] translate-y-[0.2rem] border-b-[0.04rem] border-l-[0.04rem] border-blue-400 opacity-0 transition-all duration-500 ease-out group-hover:-translate-x-[0.12rem] group-hover:translate-y-[0.08rem] group-hover:opacity-100 group-hover:blur-0"></div>
                                     <img
                                         src={item.image}
                                         className="object-contain group-hover:scale-105"
                                     ></img>
                                 </div>
-                                <div className="game-item-name relative mt-12 text-center text-[22px] font-bold">
-                                    <div className="bg-linear-to-r from-cyan-400 to-purple-400 bg-clip-text tracking-widest text-transparent drop-shadow-[0_0_10px_rgba(255,255,255,0.4)]">
-                                        <span className="font-kai text-[20px]">{item.name}</span>
-                                        <span className="font-tai ml-2 text-[14px]">
+                                <div className="game-item-name relative mt-[0.48rem] text-center text-[0.22rem] font-bold">
+                                    <div className="bg-linear-to-r from-cyan-400 to-purple-400 bg-clip-text tracking-widest text-transparent drop-shadow-[0_0_0.1rem_rgba(255,255,255,0.4)]">
+                                        <span className="font-kai text-[0.2rem]">{item.name}</span>
+                                        <span className="font-tai ml-[0.08rem] text-[14px]">
                                             {item.samelName}
                                         </span>
                                     </div>
                                     <div
-                                        className="bg-linear-to-r m-auto mt-2 max-w-max cursor-pointer whitespace-nowrap rounded-[5px] border-2 border-[#16b9ff] from-cyan-400 to-purple-400 bg-clip-text pb-2 pl-3 pr-3 pt-2 text-[16px] tracking-widest text-transparent drop-shadow-[0_0_10px_rgba(255,255,255,0.4)] active:scale-95 active:shadow-md"
+                                        className="bg-linear-to-r m-auto mt-[0.08rem] max-w-max cursor-pointer whitespace-nowrap rounded-[0.05rem] border-[0.02rem] border-[#16b9ff] from-cyan-400 to-purple-400 bg-clip-text pb-[0.08rem] pl-[0.12rem] pr-[0.12rem] pt-[0.08rem] text-[0.16rem] tracking-widest text-transparent drop-shadow-[0_0_0.1rem_rgba(255,255,255,0.4)] active:scale-95 active:shadow-md"
                                         onClick={() => gotoReceivePage(item.tag)}
                                     >
                                         卡密领取
--- card-secret-get/src/home/index.tsx
+++ card-secret-get/src/home/index.tsx
@@ -12,7 +12,7 @@ const HomePage = () => {
     return (
         <div className="home-page-background relative h-screen w-screen bg-cover bg-center bg-no-repeat text-white">
             <div className="size-container grid-rows-[1.5fr 1.5fr 1.5fr 1fr] [@media(max-height:700px)]:grid-rows-[2fr 2fr 1fr] grid h-full [@media(max-height:700px)]:grid-flow-col [@media(max-height:700px)]:grid-cols-2">
-                <div className="pt-8 [@media(max-height:700px)]:col-start-1 [@media(max-height:700px)]:col-end-3">
+                <div className="pt-[0.32rem] [@media(max-height:700px)]:col-start-1 [@media(max-height:700px)]:col-end-3">
                     <img
                         className="header-background m-auto"
                         src="/icon.png"
@@ -20,52 +20,56 @@ const HomePage = () => {
                         height="300"
                     ></img>
                 </div>
-                <div className="font-kai text-[4rem]! font-bold! flex items-center justify-center whitespace-nowrap [@media(max-height:700px)]:col-start-1">
+                <div className="font-kai font-bold! flex items-center justify-center whitespace-nowrap text-[0.64rem] [@media(max-height:700px)]:col-start-1 [@media(max-width:750px)]:text-[0.25rem]">
                     BBC-CHEAT
                 </div>
                 <div className="flex items-center justify-center [@media(max-height:700px)]:col-start-2">
                     <div
-                        className="leading-14 group relative m-auto h-14 w-32 cursor-pointer overflow-hidden bg-[#1668dc] text-center font-bold text-white active:scale-95 active:shadow-md"
+                        className="group relative m-auto h-[0.56rem] w-[1.28rem] cursor-pointer overflow-hidden bg-[#1668dc] text-center font-bold leading-[0.56rem] text-white active:scale-95 active:shadow-md"
                         onClick={() => gotoEntryPage()}
                     >
-                        <span className="font-tai relative z-10 whitespace-nowrap">进入首页</span>
+                        <div className="font-tai relative z-10 whitespace-nowrap text-[0.16rem]">
+                            进入首页
+                        </div>
                         <span className="absolute inset-0 translate-y-full bg-black/30 transition-transform duration-300 ease-linear group-hover:translate-y-0"></span>
                     </div>
                 </div>
-                <div className="m-auto flex flex-wrap items-center pb-8 pl-8 pr-8 [@media(max-height:700px)]:col-start-1 [@media(max-height:700px)]:col-end-3">
+                <div className="m-auto flex flex-wrap items-center pb-[0.32rem] pl-[0.32rem] pr-[0.32rem] [@media(max-height:700px)]:col-start-1 [@media(max-height:700px)]:col-end-3">
                     <div
-                        className="z-1 relative mb-5 ml-11 mr-11 flex cursor-pointer items-center justify-center text-start"
+                        className="z-1 relative mb-[0.2rem] ml-[0.44rem] mr-[0.44rem] flex -translate-x-[10%] cursor-pointer items-center justify-center text-start"
                         onClick={() => gotoExternal(homePageLinkUrl.tgChannel)}
                     >
-                        <img
-                            src="/tg.png"
-                            width="62"
-                            className="relative translate-x-[15%] object-contain"
-                        ></img>
-                        <div className="absolute left-[10%] top-[27%] z-[-1] h-6 w-6 translate-x-[35%] bg-white"></div>
+                        <div className="relative translate-x-[15%]">
+                            <img src="/tg.png" width="62" className="object-contain"></img>
+                            <div className="absolute left-[50%] top-[50%] z-[-1] h-[0.2rem] w-[0.2rem] -translate-x-[50%] -translate-y-[50%] bg-white"></div>
+                        </div>
                         <div>
-                            <div className="font-kai whitespace-nowrap text-[14px]">JOIN TG</div>
-                            <div className="font-tai whitespace-nowrap text-[22px]">加入TG频道</div>
+                            <div className="font-kai whitespace-nowrap text-[0.14rem]">JOIN TG</div>
+                            <div className="font-tai whitespace-nowrap text-[0.22rem]">
+                                加入TG频道
+                            </div>
                         </div>
                     </div>
                     <div
-                        className="mb-5 ml-11 mr-11 flex cursor-pointer items-center justify-start"
+                        className="mb-[0.2rem] ml-[0.44rem] mr-[0.44rem] flex cursor-pointer items-center justify-start"
                         onClick={() => gotoExternal(homePageLinkUrl.discordChannel)}
                     >
                         <img src="/discount.png" width="45" className="object-contain"></img>
                         <div>
-                            <div className="font-kai whitespace-nowrap text-[14px]">
+                            <div className="font-kai whitespace-nowrap text-[0.14rem]">
                                 JOIN DISCORD
                             </div>
-                            <div className="font-tai whitespace-nowrap text-[22px]">加入DISCRD</div>
+                            <div className="font-tai whitespace-nowrap text-[0.22rem]">
+                                加入DISCRD
+                            </div>
                         </div>
                     </div>
                     <div
-                        className="mb-5 ml-11 mr-11 cursor-pointer whitespace-nowrap"
+                        className="mb-[0.2rem] ml-[0.44rem] mr-[0.44rem] cursor-pointer whitespace-nowrap"
                         onClick={() => gotoExternal(homePageLinkUrl.downloadAddress)}
                     >
-                        <div className="font-kai whitespace-nowrap text-[14px]">DOWNLOAD</div>
-                        <div className="font-tai whitespace-nowrap text-[22px]">下载地址</div>
+                        <div className="font-kai whitespace-nowrap text-[0.14rem]">DOWNLOAD</div>
+                        <div className="font-tai whitespace-nowrap text-[0.22rem]">下载地址</div>
                     </div>
                     {/* <div className="mr-16 whitespace-nowrap">常见问题</div> */}
                 </div>
