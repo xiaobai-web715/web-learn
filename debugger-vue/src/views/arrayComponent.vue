572232635599cada747368757d6f9faddaf36d35
刘兴华 <liuxinghua@edianyun.com>
2025-12-08 09:57:12 +0800

    ✨ feat: 使用v-for触发diff算法

---
+++ debugger-vue/src/views/arrayComponent.vue
@@ -0,0 +1,16 @@
+<template>
+  <div v-for="(item) in arrayList" :key="item">
+    {{  item }}
+  </div>
+</template>
+<script setup lang="ts">
+import { computed } from "vue"
+const { value } = defineProps<{
+  value: number
+}>()
+const arrayList = computed(() => {
+  return Array.from({length: 10}).map((_, index) => {
+    return value * 10 + index
+  })
+})
+</script>
--- debugger-vue/src/views/testComponent.vue
+++ debugger-vue/src/views/testComponent.vue
@@ -1,13 +1,11 @@
 <template>
-  <div>doubleCount is : {{ doubleCount }}</div>
   <button @click="updateCount">点击增加功能</button>
+  <arrayComponent :value="count"></arrayComponent>
 </template>
 <script lang="ts" setup>
-import { ref, computed } from 'vue'
-debugger
-console.log("开始执行testComputed的值")
+import { ref } from 'vue'
+import arrayComponent from './arrayComponent.vue';
 const count = ref(0)
-const doubleCount = computed(() => count.value * 2)
 function updateCount() {
   count.value++;
 }
