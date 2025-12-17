ec456a4dc5a15afdeb04b7b67e1e394dda002c2a
刘兴华 <liuxinghua@edianyun.com>
2025-12-17 10:52:06 +0800

    ✨ feat:修改异步加载组件关闭弹窗立即消失没有动画的效果

--- vue-vite/src/view/test-defineAsyncComponent/index.vue
+++ vue-vite/src/view/test-defineAsyncComponent/index.vue
@@ -1,12 +1,14 @@
 <template>
     <a-button @click="openModal">打开弹窗</a-button>
-    <Modal v-model:visible="visible" v-if="visible"></Modal>
+    <Modal v-model:visible="visible" v-if="modalLoaded"></Modal>
 </template>
 <script lang="ts" setup>
 import { ref, defineAsyncComponent } from 'vue';
 const visible = ref<boolean>(false);
+const modalLoaded = ref<boolean>(false);
 const Modal = defineAsyncComponent(() => import('./dialog.vue'));
 const openModal = () => {
     visible.value = true;
+    modalLoaded.value = true;
 };
 </script>
